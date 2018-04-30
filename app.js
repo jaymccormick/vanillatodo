6b31d0a372f1d931db16c11435a9a74b94d81ed5
jaymccormick <jaymccormi@gmail.com>
2018-04-30 12:21:16 -0400

    create list items function

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const form = document.getElementById('addItems');
 const input = form.querySelector('input');
 const ul = document.getElementById('todoItems');
 
+function createLI(text){
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  span.textContent = text;
+  li.appendChild(span);
+  return li;
+}
+
 form.addEventListener('submit', (e) =>{
   e.preventDefault();
   const text = input.value;
